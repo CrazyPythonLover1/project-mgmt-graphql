9486cb7bec6fa636fd6c5612722f329cbc538006
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2023-03-13 05:57:17 -0400

    Done, All Project deleted if client deleted

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container">
-        <a className="navbar-brand" href="#">Project Management</a>
+        <a className="navbar-brand" href="/">Project Management</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -12,7 +12,7 @@ export default function Header() {
           <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" 
           >
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             {/* <li className="nav-item">
               <a className="nav-link" href="#">Link</a>
